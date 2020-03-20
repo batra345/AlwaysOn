0d503627047eeb671d2dde946aaed60523b83be0
MOHIT BATRA <batracloud345@gmail.com>
2020-03-20 14:20:22 +0000

    Updated attach-NSG.ps1

--- Scripts/attach-NSG.ps1
+++ Scripts/attach-NSG.ps1
@@ -24,6 +24,10 @@ $objParametersFile = Get-Content -Path $ParametersFile | ConvertFrom-Json
 [System.String] $NICname = $objParametersFile.parameters.NICname[0].value
 [System.String] $NSGname = $objParametersFile.parameters.NSGname[0].value
 
+$rules = New-AzNetworkSecurityRuleConfig -Name 'default-allow-rdp' -Direction Inbound -Priority 1000 -Access Allow -SourceAddressPrefix '*'  -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange 3389 -Protocol Tcp 
+New-AzNetworkSecurityGroup -Name "DB-NSG" -ResourceGroupName $ResourceGroupName -Location "westeurope" -SecurityRules $rules
+
+
 $nic = Get-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name $NICname
 $nsg = Get-AzNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Name $NSGname
 $nic.NetworkSecurityGroup = $nsg
