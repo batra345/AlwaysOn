2a67fc09c289c6be8be1a7cfe96715da8056d926
MOHIT BATRA <batracloud345@gmail.com>
2020-03-21 13:54:28 +0000

    Updated create-attach-NSG.ps1

--- Scripts/create-attach-NSG.ps1
+++ Scripts/create-attach-NSG.ps1
@@ -19,15 +19,19 @@ param
     
 )
 
+#Read parameter file and assign variables
 $objParametersFile = Get-Content -Path $ParametersFile | ConvertFrom-Json
 [System.String] $ResourceGroupName = $objParametersFile.parameters.RGname[0].value
 [System.String] $NICname = $objParametersFile.parameters.NICname[0].value
-[System.String] $NSGname = $objParametersFile.parameters.NSGname[0].value
+[System.String] $NSGname  = $objParametersFile.parameters.NSGname[0].value
+[System.String] $Location1  = $objParametersFile.parameters.Location1[0].value
+[System.String] $RuleName1  = $objParametersFile.parameters.RuleName1[0].value
 
-$rules = New-AzNetworkSecurityRuleConfig -Name 'default-allow-rdp' -Direction Inbound -Priority 1000 -Access Allow -SourceAddressPrefix '*'  -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange 3389 -Protocol Tcp 
-New-AzNetworkSecurityGroup -Name "DB-NSG" -ResourceGroupName $ResourceGroupName -Location "westeurope" -SecurityRules $rules
+#Define rule, create NSG and attach rule
+$rules = New-AzNetworkSecurityRuleConfig -Name $RuleName1 -Direction Inbound -Priority 1000 -Access Allow -SourceAddressPrefix '*'  -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange 3389 -Protocol Tcp 
+New-AzNetworkSecurityGroup -Name $NSGname -ResourceGroupName $ResourceGroupName -Location $Location1 -SecurityRules $rules
 
-
+#Attach NSG to NIC
 $nic = Get-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name $NICname
 $nsg = Get-AzNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Name $NSGname
 $nic.NetworkSecurityGroup = $nsg
